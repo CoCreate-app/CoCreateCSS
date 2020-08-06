a8f2fe6226265a585a4315255d72c6796373759c
Frank Pagan <frank@cocreate.app>
2020-08-06 22:05:02 +0000

    add css parse demo files

---
+++ css_parse/parsing3.js
@@ -0,0 +1,95 @@
+let linklength = document.head.querySelectorAll("link[rel=stylesheet]").length;
+let link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+document.head.querySelectorAll("link[rel=stylesheet]")[linklength - 1].insertAdjacentHTML('afterend', link.outerHTML);
+var helperClassList = [];
+var myStyle;
+myStyle = document.styleSheets[linklength];
+let elements = document.querySelectorAll("[class]");
+for (let element of elements) {
+    addParsingClassList(element.classList);
+}
+const ob_config = { attributes: true, childList: false, subtree: true };
+const callback = function(mutationsList, observer) {
+    let myStyle = document.styleSheets[linklength];
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === "class") {
+            addParsingClassList(mutation.target.classList);
+        }
+    }
+};
+const observer = new MutationObserver(callback);
+observer.observe(document.body, ob_config);
+function addParsingClassList(classList) {
+    let re = /.+:.+/;
+    for (let classname of classList) {
+        if (re.exec(classname)) {
+            if (helperClassList.indexOf(classname) == -1) {
+                let re_at = /.+@.+/;
+                if (re_at.exec(classname)) {
+                    let parts = classname.split("@");
+                    let main_rule = parseClass(classname);
+                    const range_names = ["xs", "sm", "md", "lg"];
+                    const ranges = [[0, 767], [768, 991], [992, 1199], [1200, 0]];
+                    for (let i = 1; i < parts.length; i++) {
+                        let range_num = range_names.indexOf(parts[i]);
+                        if (range_num == -1) continue;
+                        let range = ranges[range_num];
+                        let prefix = "@media screen";
+                        if (range[0] != 0) {
+                            prefix += " and (min-width:" + range[0] + "px)";
+                        }
+                        if (range[1] != 0) {
+                            prefix += " and (max-width:" + range[1] + "px)";
+                        }
+                        let rule = prefix + "{" + main_rule + "}";
+                        myStyle.insertRule(rule);
+                        helperClassList.push(classname);
+                    }
+                } else {
+                    let re_at = new RegExp("^" + classname + "@");
+                    let rulelist = [];
+                    let index = helperClassList.findIndex(value => re_at.test(value));
+                    while(index != -1) {
+                        rulelist.push(myStyle.cssRules[index]);
+                        helperClassList.splice(index, 1);
+                        myStyle.deleteRule(index);
+                        index = helperClassList.findIndex(value => re_at.test(value));
+                    }
+                    let rule = parseClass(classname)
+                    myStyle.insertRule(rule);
+                    helperClassList.push(classname);
+                    if (rulelist.length > 0) {
+                        for (let i = 0; i < rulelist.length; i++) {
+                            myStyle.insertRule(rulelist[i].selectorText + rulelist[i].cssText);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    function parseClass(classname) {
+        let res = classname.split(":");
+        let rule = "";
+        let suffix = res[1].replace(/\./g, "\\.").replace(/%/, "\\%").replace(/@/g, "\\@");
+        res[1] = res[1].split("@")[0];
+        res[1] = res[1].replace(/_/g, " ");
+        if (res.length > 2) {
+            let pseudo = [];
+            for (let i = 0; i < res.length - 2; i++) {
+                suffix += "\\:" + res[2 + i];
+                pseudo.push(":" + res[2 + i]);
+            }
+            let clsname = "." + res[0] + "\\:" + suffix;
+            rule += clsname + pseudo[0];
+            for (let i = 1; i < pseudo.length; i++) {
+                rule += ", " + clsname + pseudo[i];
+            }
+            rule += `{${res[0]}:${res[1]}}`;
+        } else {
+            rule = `.${res[0]}\\:${suffix}{${res[0]}:${res[1]}}`;
+        }
+        return rule;
+    }
+}
