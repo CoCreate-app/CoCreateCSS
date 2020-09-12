b8cb2755757f43a0d1ca9b66cbe12cd0a6dee7d8
Frank Pagan <frank@cocreate.app>
2020-09-12 03:05:08 +0000

    update parsing css for special characters

--- css_parse/parsing.js
+++ css_parse/parsing.js
@@ -61,43 +61,57 @@ function sortRules(){
 function addParsingClassList(classList) {
     let re = /.+:.+/;
     for (let classname of classList) {
-        if (re.exec(classname)) {
-            if (utilityClassList.indexOf(classname) == -1) {
-                let re_at = /.+@.+/;
-                if (re_at.exec(classname)) {
-                    let parts = classname.split("@");
-                    let main_rule = parseClass(classname);
-                    const range_names = ["xs", "sm", "md", "lg"];
-                    const ranges = [[0, 767], [768, 991], [992, 1199], [1200, 0]];
-                    for (let i = 1; i < parts.length; i++) {
-                        let range_num = range_names.indexOf(parts[i]);
-                        if (range_num == -1) continue;
-                        let range = ranges[range_num];
-                        let prefix = "@media screen";
-                        if (range[0] != 0) {
-                            prefix += " and (min-width:" + range[0] + "px)";
+        try {
+            if (re.exec(classname)) {
+                if (utilityClassList.indexOf(classname) == -1) {
+                    // let re_important = /.*!important.*/;
+                    // let important = 0;
+                    // if (re_important.exec(classname)) {
+                    //     console.log("important");
+                    //     important = 1;
+                    //    let temp = classname.split("!important");
+                    //    classname = "";
+                    //    for (let tmp of temp) {
+                    //        classname += tmp;
+                    //    }
+                    // }
+                    let re_at = /.+@.+/;
+                    if (re_at.exec(classname)) {
+                        let parts = classname.split("@");
+                        let main_rule = parseClass(classname);
+                        const range_names = ["xs", "sm", "md", "lg"];
+                        const ranges = [[0, 767], [768, 991], [992, 1199], [1200, 0]];
+                        for (let i = 1; i < parts.length; i++) {
+                            let range_num = range_names.indexOf(parts[i]);
+                            if (range_num == -1) continue;
+                            let range = ranges[range_num];
+                            let prefix = "@media screen";
+                            if (range[0] != 0) {
+                                prefix += " and (min-width:" + range[0] + "px)";
+                            }
+                            if (range[1] != 0) {
+                                prefix += " and (max-width:" + range[1] + "px)";
+                            }
+                            let rule = prefix + "{" + main_rule + "}";
+                            myStyle.insertRule(rule);
+                            utilityClassList.push(classname);
                         }
-                        if (range[1] != 0) {
-                            prefix += " and (max-width:" + range[1] + "px)";
-                        }
-                        let rule = prefix + "{" + main_rule + "}";
+                    } else {
+                        let rule = parseClass(classname);
                         myStyle.insertRule(rule);
                         utilityClassList.push(classname);
                     }
-                } else {
-                    let rule = parseClass(classname);
-                    myStyle.insertRule(rule);
-                    utilityClassList.push(classname);
                 }
             }
         }
+        catch(e){}
     }
 }
 
 function parseClass(classname) {
     let res = classname.split(":");
     let rule = "";
-    let suffix = res[1].replace(/\./g, "\\.").replace(/%/, "\\%").replace(/@/g, "\\@");
+    let suffix = res[1].replace(/\./g, "\\.").replace(/%/, "\\%").replace(/@/g, "\\@").replace(/\(/g, "\\(").replace(/\)/g, "\\)").replace(/#/g, "\\#").replace(/,/g, "\\,").replace(/!/,"\\!");
     res[1] = res[1].split("@")[0];
     res[1] = res[1].replace(/_/g, " ");
     if (res.length > 2) {
@@ -113,7 +127,7 @@ function parseClass(classname) {
         }
         rule += `{${res[0]}:${res[1]}}`;
     } else {
-        rule = `.${res[0]}\\:${suffix}{${res[0]}:${res[1]} !important}`;
+        rule = `.${res[0]}\\:${suffix}{${res[0]}:${res[1]}}`;
     }
     return rule;
 }
