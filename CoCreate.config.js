b453c32ebd2c4a5b6a28da01ec7fa517b46fceb8
frank <frank@cocreate.app>
2021-05-03 01:19:02 +0000

    docs: CoCreate-hosting package to publish docs

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,35 +1,57 @@
 module.exports = {
     config: {
         apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-        securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
         organization_Id: "5de0387b12e200ea63204d6c",
         host: "server.cocreate.app:8088"
     },
  
-    sources: [{
-            path: "./docs/index.html",
+    sources: [
+        {
+            entry: "./docs/index.html",
             collection: "files",
             document_id: "60132e7286c0ba24512cca3c",
-            key: "html",
+            key: "src",
             data:{
                 name: "CoCreateCSS Doc",
+                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                path: "/docs/CoCreateCSS",
+            }
+        },
+        {
+            entry: "./docs/utility.html",
+            collection: "files",
+            document_id: "60132e7286c0ba24512cca3c",
+            key: "src",
+            data:{
+                name: "Utility Doc",
+                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                path: "/docs/utility",
+            }
+        },
+        {
+            entry: "./docs/avatar.html",
+            collection: "files",
+            document_id: "60132e7286c0ba24512cca3c",
+            key: "src",
+            data:{
+                name: "Avatar Doc",
+                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                path: "/docs/avatar",
+            }
+        },
+        {
+            entry: "./docs/flip-item.html",
+            collection: "files",
+            document_id: "60132e7286c0ba24512cca3c",
+            key: "src",
+            data:{
+                name: "Flip Item Doc",
+                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                path: "/docs/flip-item",
             }
         },
     ],
    
-    crud: [{
-            collection: "routes",
-            document_id: "60132e7286c0ba24512cca3b",
-            data:{
-                collection: "files",
-                document_id: "60132e7286c0ba24512cca3c",
-                name: "html",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                route: "/docs/CoCreateCSS",
-            }
-        }
-    ],
-    
     extract: {
         directory: "./src/",
         extensions: [
