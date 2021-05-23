7071f3acfebdee830e3e888647d2426752a1166f
frank <frank@cocreate.app>
2021-05-23 03:52:02 +0000

    update test

--- src/test.js
+++
@@ -1,9 +0,0 @@
-function addRule(stylename, selector, rule) {
-
-    var stylesheet = document.querySelector(`link[data-save]`)
-    console.log('stylesheet', stylesheet)
-
-    let myRules = stylesheet.cssRules; // Returns a CSSRuleList
-    console.log(myRules); // let ruleList = stylesheet.cssRules;
-}
-// example use
