2c9dd05d484e418e5ea2d5867300cf0f867a73ea
frank <frank@cocreate.app>
2021-06-13 23:46:51 +0000

    docs

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,69 +1,69 @@
 module.exports = {
-    config: {
-        apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-        organization_Id: "5de0387b12e200ea63204d6c",
-        host: "server.cocreate.app:8088"
+    "config": {
+        "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
+        "organization_Id": "5de0387b12e200ea63204d6c",
+        "host": "server.cocreate.app:8088"
     },
- 
-    sources: [
+    "sources": [
         {
-            entry: "./docs/index.html",
-            collection: "files",
-            document_id: "60132e7286c0ba24512cca3c",
-            key: "src",
-            data:{
-                name: "CoCreateCSS Doc",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                path: "/docs/CoCreateCSS",
+            "entry": "./docs/index.html",
+            "collection": "files",
+            "document_id": "60132e7286c0ba24512cca3c",
+            "key": "src",
+            "data": {
+                "name": "CoCreateCSS Doc",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/docs/CoCreateCSS"
             }
         },
         {
-            entry: "./docs/utility.html",
-            collection: "files",
-            document_id: "60132e7286c0ba24512cca3c",
-            key: "src",
-            data:{
-                name: "Utility Doc",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                path: "/docs/utility",
+            "entry": "./docs/utility.html",
+            "collection": "files",
+            "document_id": "60c63f2429e7550bebd6fb88",
+            "key": "src",
+            "data": {
+                "name": "Utility Doc",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/docs/utility"
             }
         },
         {
-            entry: "./docs/avatar.html",
-            collection: "files",
-            document_id: "60132e7286c0ba24512cca3c",
-            key: "src",
-            data:{
-                name: "Avatar Doc",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                path: "/docs/avatar",
+            "entry": "./docs/avatar.html",
+            "collection": "files",
+            "document_id": "60c63f2429e7550bebd6fb89",
+            "key": "src",
+            "data": {
+                "name": "Avatar Doc",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/docs/avatar"
             }
         },
         {
-            entry: "./docs/flip-item.html",
-            collection: "files",
-            document_id: "60132e7286c0ba24512cca3c",
-            key: "src",
-            data:{
-                name: "Flip Item Doc",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                path: "/docs/flip-item",
+            "entry": "./docs/flip-item.html",
+            "collection": "files",
+            "document_id": "60c63f2429e7550bebd6fb8a",
+            "key": "src",
+            "data": {
+                "name": "Flip Item Doc",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/docs/flip-item"
             }
-        },
-    ],
-   
-    extract: {
-        directory: "./src/",
-        extensions: [
-            "js",
-            "css",
-            "html"
-        ],
-        ignores: [
-            "node_modules",
-            "vendor",
-            "bower_components",
-            "archive"
-        ],
-    }
+        }
+    ]
 }
