0b9b8e0e1d8588c0c0bc59f771bbec09d20eb023
Frank Pagan <frank@cocreate.app>
2022-12-13 14:28:58 +0000

    fix: add min.css to /latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path")
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 let isProduction = process.env.NODE_ENV === "production"
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
   entry: {
@@ -11,23 +14,21 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: isProduction ? "[name].min.js" : "[name].js",
+    chunkFilename: isProduction ? '[name].min.js' : '[name].js',
     libraryTarget: "umd",
     libraryExport: "default",
     library: ["CoCreate", "css"],
     globalObject: "this",
-    // publicPath: 'https://server.cocreate.app/CoCreateJS/dist/'
+    ...(isProduction ? { /*publicPath: 'https://cdn.cocreate.app/',*/ } : {}),
   },
 
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+        filename: isProduction ? 'CoCreate.min.css' : 'CoCreate.css',
     }),
   ],
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
+
   mode: isProduction ? "production" : "development",
   module: {
     rules: [
@@ -44,10 +45,9 @@ module.exports = {
       {
         test: /.css$/i,
         use: [
-          { loader: "style-loader", options: { injectType: "linkTag" } },
-          "file-loader",
-        ],
-      // use: [MiniCssExtractPlugin.loader,    { loader: "style-loader", options: { injectType: "linkTag" } }],
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       },
     ],
   },
@@ -58,29 +58,18 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
+      new CssMinimizerPlugin(),
       new TerserPlugin({
         extractComments: true,
         // cache: true,
         parallel: true,
         // sourceMap: true, // Must be set to true if using source-maps in production
         terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          // extractComments: 'all',
           compress: {
             drop_console: true,
           },
         },
       }),
-    ],
-    splitChunks: {
-      chunks: "all",
-      minSize: 200,
-      // maxSize: 99999,
-      //minChunks: 1,
-
-      cacheGroups: {
-        defaultVendors: false,
-      },
-    },
+    ]
   },
 }
