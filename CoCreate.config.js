3aeeeb9d7bf00d89916a65ec1954d77d144a43bb
frankpagan <frank@cocreate.app>
2023-05-01 07:14:47 -0600

    parentDirectory

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,57 +4,26 @@ module.exports = {
         "apiKey": "2061acef-0451-4545-f754-60cf8160",
         "host": "general.cocreate.app"
     },
-    "sources": [
+    "directories": [
         {
+            "entry": "./docs",
+            "exclude": [
+                "demo"
+            ],
             "collection": "files",
             "document": {
-                "_id": "60c63f2429e7550bebd6fb88",
-                "name": "index.html",
-                "path": "/docs/utility/index.html",
-                "src": "{{./docs/utility.html}}",
+                "name": "{{name}}",
+                "src": "{{source}}",
                 "hosts": [
                     "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs",
-                "content-type": "text/html",
+                "parentDirectory": "docs",
+                "path": "{{path}}",
+                "content-type": "{{content-type}}",
                 "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "collection": "files",
-            "document": {
-                "_id": "60c63f2429e7550bebd6fb89",
-                "name": "avatar.html",
-                "path": "/docs/avatar/index.html",
-                "src": "{{./docs/avatar.html}}",
-                "hosts": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "",
-            "collection": "files",
-            "document": {
-                "_id": "60c63f2429e7550bebd6fb8a",
-                "name": "flip-item.html",
-                "path": "/docs/flip-item/index.html",
-                "src": "{{./docs/flip-item.html}}",
-                "hosts": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
+                "website_id": "644d4bff8036fb9d1d1fd69c"
             }
         }
     ]
