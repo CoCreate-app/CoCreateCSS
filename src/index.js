67955e762d3ba1c65f26b8dfae8af12715ba734d
frankpagan <frank@cocreate.app>
2023-11-07 12:48:42 -0600

    fix: removed button and dropdown

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './css/avatar.css';
 import './css/badge.css';
 import './css/box-shadow.css';
-import './css/button.css';
 import './css/card.css';
 import './css/checkbox.css';
 import './css/core.css';
-import './css/dropdown.css';
 import './css/flip-item.css';
 import './css/menu-icon.css';
 import './css/navbar.css';
